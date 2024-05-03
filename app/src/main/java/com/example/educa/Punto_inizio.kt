203c577f663e1983b8b4b36ffadcac2e5046dd9c
emanueleluraschi <luraschiemanuele@gmail.com>
2024-05-03 08:53:32 +0200

    refactor

--- app/src/main/java/com/example/educa/Punto_inizio.kt
+++ app/src/main/java/com/example/educa/Punto_inizio.kt
@@ -16,5 +16,6 @@ class Punto_inizio : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
     }
 }
