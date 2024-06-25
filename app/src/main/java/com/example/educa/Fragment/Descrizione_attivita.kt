4362018ada4e84402059a8f7aacd334b63a4d2fe
emanueleluraschi <luraschiemanuele@gmail.com>
2024-06-25 17:07:19 +0200

    aggiunta descrizione all'attività(cliccando si entra dentro atttività)

--- app/src/main/java/com/example/educa/Fragment/Descrizione_attivita.kt
+++ app/src/main/java/com/example/educa/Fragment/Descrizione_attivita.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import com.example.educa.R
+import com.example.educa.SharedViewModel
 
 
 class Descrizione_attivita : Fragment() {
@@ -17,8 +19,30 @@ class Descrizione_attivita : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_descrizione_attivita, container, false)
+
+        val titoloTextView = view.findViewById<TextView>(R.id.activity_details_title)
+        val etaTextView = view.findViewById<TextView>(R.id.activity_details_age)
+        val durataTextView = view.findViewById<TextView>(R.id.activity_details_duration)
+        val modalitaTextView = view.findViewById<TextView>(R.id.activity_details_modality)
+        val descrizioneTextView = view.findViewById<TextView>(R.id.activity_details_description)
+
+        SharedViewModel.selectedActivity.observe(viewLifecycleOwner) { activity ->
+        activity?.let {
+            titoloTextView.text = it.name
+            etaTextView.text = "età: "+ it.age.toString()
+            durataTextView.text ="durata: " +it.duration.toString()
+            modalitaTextView.text = "modalità: "+when{
+                it.isForSingle->" Singolo"
+                it.isForGroup->" Gruppo"
+                else->" Ambito"
+            }
+            descrizioneTextView.text = it.descriptionLong
+            }
+        }
+
         return view
     }
+
 
 
 }
